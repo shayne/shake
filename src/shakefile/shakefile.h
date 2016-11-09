95e899c29d48674ab66a2450f2b0ab94df329b5f
Shayne Sweeney <shaynesweeney@me.com>
2016-11-09 13:07:57 -0800

    General cleanup

--- src/shakefile/shakefile.h
+++ src/shakefile/shakefile.h
@@ -5,16 +5,6 @@
 #ifndef SHAKE_SHAKEFILE_H
 #define SHAKE_SHAKEFILE_H
 
-#include "../utils.h"
-
-typedef struct {
-    char *file_path;
-    char *root_path;
-} Shakefile;
-
-Shakefile *Shakefile_create();
-void Shakefile_destroy(Shakefile *file);
-
 char *Shakefile_find_projfile(int *success);
 int Shakefile_detect_functions(char *projfile, size_t size, char *fns[]);
 
